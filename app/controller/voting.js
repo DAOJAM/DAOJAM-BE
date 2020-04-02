5c1e0cd003236576198ddd6d46efe655207aa4e5
shellteo <shellteo@163.com>
2020-04-02 09:01:33 +0800

    feat: add mint log & add voting details

--- app/controller/voting.js
+++ app/controller/voting.js
@@ -12,10 +12,33 @@ class VotingController extends Controller {
     // const { address } = ctx.request.body;
     const amount = 100; // 每天可领赠票的数量
     const user = await this.service.account.binding.get(ctx.user.id, 'eth');
+    if (!user) {
+      ctx.body = ctx.msg.failure;
+      return;
+    }
     const result = await this.service.voting._mint(user.account, amount);
+    await this.service.mintLog.create({
+      uid: ctx.user.id, address: user.account, daot: amount, block_number: result.blockNumber, trx: result.transactionHash,
+    });
     ctx.body = {
       ...ctx.msg.success,
       data: result,
+    };
+  }
+  async balance() {
+    const ctx = this.ctx;
+    const user = await this.service.account.binding.get(ctx.user.id, 'eth');
+    if (!user) {
+      ctx.body = {
+        ...ctx.msg.success,
+        data: 0,
+      };
+      return;
+    }
+    const balance = await this.service.voting.balance(user.account);
+    ctx.body = {
+      ...ctx.msg.success,
+      data: balance,
     };
   }
 }
