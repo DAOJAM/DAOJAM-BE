0511cb4630e9857958468b5bcf6c9598c50e3c3f
xiaotian <952822399@qq.com>
2020-04-03 19:47:10 +0800

    团队任务设置

--- app/controller/task.js
+++ app/controller/task.js
@@ -10,6 +10,42 @@ class TaskController extends Controller {
       data: result
     };
   }
+
+  async taskTeam() {
+    const { ctx } = this;
+    const userId = ctx.user.id;
+    if (userId) {
+      const result = await this.service.task.taskTeam(userId);
+      ctx.body = {
+        ...this.ctx.msg.success,
+        data: result
+      };
+    } else {
+      ctx.body = ctx.msg.failure;
+    }
+  }
+
+  async updateTask() {
+    const ctx = this.ctx;
+    const { task } = this.ctx.request.body;
+    const userId = ctx.user.id;
+    if (userId) {
+      const result = await ctx.service.task.updateTask(userId, task);
+      if (result.code === 0) {
+        ctx.body = {
+          ...ctx.msg.success,
+          data: result.data,
+        };
+      } else {
+        ctx.body = ctx.msg.failure;
+      }
+      if (result.message) {
+        ctx.body.message = result.message;
+      }
+    } else {
+      ctx.body = ctx.msg.failure;
+    }
+  }
 }
 
 module.exports = TaskController;
