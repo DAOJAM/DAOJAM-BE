618dd2cdc699efbf52a79668f46c863069599820
xiaotian <952822399@qq.com>
2020-04-10 20:11:58 +0800

    排行接口

---
+++ app/service/leaderboard.js
@@ -0,0 +1,20 @@
+'use strict';
+const Service = require('egg').Service;
+
+class LeaderboardService extends Service {
+  async all() {
+    try {
+      const sql = `SELECT d.pid, d.uid, SUM(d.weight) AS weight, SUM(POW(d.weight,2)) AS daot, m.logo, m.name
+      FROM daojam_vote_log d, minetokens m
+      WHERE d.pid = m.pid
+      GROUP BY pid ORDER BY weight DESC, daot DESC;`;
+
+      return await this.app.mysql.query(sql);
+    } catch (e) {
+      this.ctx.logger.error(`task fail: ${e}`);
+      return [];
+    }
+  }
+}
+
+module.exports = LeaderboardService;
